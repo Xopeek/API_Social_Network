6ba7359616e5325f917f36c8cabe7bc2d6681518
Игорь Семляков <sream6222@gmail.com>
2023-04-01 19:19:10 +0300

    v 1.0

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -11,6 +11,6 @@ class OnlyReadOrAuthor(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
